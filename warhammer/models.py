6018e0207096ef4355808150089efbc457d16526
zyvik <pawel86gw2@gmail.com>
2020-06-01 17:07:05 +0200

    Remove all 'import *'

--- warhammer/models.py
+++ warhammer/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 import uuid
+from django.db import models
 from django.contrib.auth.models import User
 
 
--- wh_chat/admin.py
+++ wh_chat/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import GameModel, MessagesModel, MapModel
-# Register your models here.
+
+
 admin.site.register(GameModel)
 admin.site.register(MessagesModel)
 admin.site.register(MapModel)
